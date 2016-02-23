f4b7d23780592668ec3b91a052fb18c886edc26b
Richard Hamilton <richardgregoryhamilton@gmail.com>
2016-02-23 17:02:47 -0600

    improve directory structure

--- server.js
+++ server.js
@@ -10,36 +10,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
 var session = require('express-session');
 
-//var configDB = require('./config/db.js');
-
 app.set("port", process.env.PORT || 3000);
-
-/*require('./config/passport')(passport);
-
-var LocalStrategy = require('passport-local').Strategy;
-
-app.use(function (req, res, next) {
-    if (req.url.match(/^\/(css|js|img|font)\/.+/)) {
-        res.setHeader('Cache-Control', 'public, max-age=3600');
-    }
-    next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({ secret: 'mysecret',
-                  saveUninitialized: true,
-                  resave: true}));
-                  
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-var routes = require('./routes')(app, passport);*/
 
 var views = __dirname + "/views";
 var partials = views + "/partials";
 var scripts = __dirname + "/scripts";
+var vendor = scripts + "/vendor";
 
 app.use(express.static(__dirname + '/images'));
 
@@ -87,10 +63,6 @@ app.get("*/images/enemy.png", function(req, res) {
     res.sendFile(__dirname + "/images/enemy.png");
 });
 
-app.get("*/images/skeleton.png", function(req, res) {
-    res.sendFile(__dirname + "/images/skeleton.png");
-});
-
 app.get("*/bootstrap.min.css", function(req,res) {
     res.sendFile(__dirname + "/styles/bootstrap.min.css");
 });
@@ -112,7 +84,7 @@ app.get("/scripts/main.js", function(req, res) {
 });
 
 app.get("/scripts/pong.js", function(req, res) {
-	res.sendFile(scripts + "/pong.js");
+    res.sendFile(scripts + "/pong.js");
 });
 
 app.get("/scripts/services/game.js", function(req, res) {
@@ -139,33 +111,25 @@ app.get("/scripts/controllers/shopController.js", function(req, res) {
     res.sendFile(scripts + "/controllers/shopController.js");
 });
 
-app.get("/scripts/lodash.js", function(req, res) {
-    res.sendFile(scripts + "/lodash.js");
+app.get("/scripts/vendor/lodash.js", function(req, res) {
+    res.sendFile(vendor + "/lodash.js");
 });
 
-app.get("/scripts/thenby.min.js", function(req, res) {
-    res.sendFile(scripts + "/thenby.min.js");
+app.get("/scripts/vendor/thenby.min.js", function(req, res) {
+    res.sendFile(vendor + "/thenby.min.js");
 });
 
-app.get("/scripts/jquery.min.js", function(req, res) {
-    res.sendFile(scripts + "/jquery.min.js");
+app.get("/scripts/vendor/jquery.min.js", function(req, res) {
+    res.sendFile(vendor + "/jquery.min.js");
 });
 
-app.get("/scripts/angular.min.js", function(req, res) {
-    res.sendFile(scripts + "/angular.min.js");
+app.get("/scripts/vendor/angular.min.js", function(req, res) {
+    res.sendFile(vendor + "/angular.min.js");
 });
 
-app.get("/scripts/jcanvas.min.js", function(req, res) {
-    res.sendFile(scripts + "/jcanvas.min.js");
+app.get("/scripts/vendor/jcanvas.min.js", function(req, res) {
+    res.sendFile(vendor + "/jcanvas.min.js");
 });
-
-/*app.get("/images/avoider.png", function(req, res) {
-    res.sendFile(images + "/avoider.png");
-});
-
-app.get("/images/enemy.png", function(req, res) {
-    res.sendFile(images + "/enemy.png");
-});*/
 
 app.listen(app.get("port"), function() {
     console.log("Server started: http://localhost:" + app.get("port") + "/");
